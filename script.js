9eb27a9d026bb721c168ddd782e04aeca88b95fa
Mandisa Reed <mandisa.reed@gmail.com>
2020-06-11 18:33:26 -0700

    created prompt for amount of pw characters

--- script.js
+++ script.js
@@ -9,5 +9,19 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+
+
+// pseudo code
+//    WHEN user clicks on Generate PW button, 
+//    PROMPT user to input how many characters they want their pw to be. 
+
+function showAlert () {
+  var userChars = prompt("How many characters do you want in your password? Your password must have no less than 8 characters and no more than 128 characters.");
+  (userChars);
+}
+
+  generateBtn.addEventListener("click", showAlert);
+
+
+  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
